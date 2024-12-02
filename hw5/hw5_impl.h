b2a2836627ed2ff86017172faffb836c374f2389
Joel Miguel <joel.miguel@utec.edu.pe>
2024-12-01 19:21:28 -0500

    desarrollado

--- hw5/hw5_impl.h
+++ hw5/hw5_impl.h
@@ -7,42 +7,86 @@
 #include "hw5.h"
 
 void add_edge(std::vector<std::vector<int>>& graph, int from, int to) {
-  // COMPLETE AQUI
+  graph[from].push_back(to); 
+  graph[to].push_back(from);
 }
 
+
 int n_vertices(std::vector<std::vector<int>> const& graph) {
-  // COMPLETE AQUI
-  return 0;
+  return graph.size();
 }
 
 int n_edges(std::vector<std::vector<int>> const& graph) {
-  // COMPLETE AQUI
-  return 0;
+  int edges = 0;
+  for (const auto& adj_list : graph) {
+    edges += adj_list.size(); 
+  }
+  return edges / 2; 
 }
 
 std::vector<int> BFS(std::vector<std::vector<int>> const& graph, int from) {
-  // COMPLETE AQUI
-  std::vector<int> ret;
+  std::vector<bool> visited(graph.size(), false);
+  std::vector<int> ret; 
+  std::queue<int> q;
+  
+  visited[from] = true;
+  q.push(from);
+  
+  while (!q.empty()) {
+    int current = q.front();
+    q.pop();
+    ret.push_back(current); 
+    
+  for (int neighbor : graph[current]) {
+    if (!visited[neighbor]) {
+        visited[neighbor] = true;
+        q.push(neighbor);
+      }
+    }
+  }
+  
   return ret;
+}
+
+
+void DFS_helper(const std::vector<std::vector<int>>& graph, int from, std::vector<bool>& visited, std::vector<int>& ret) {
+  visited[from] = true;
+  ret.push_back(from); 
+  
+  for (int neighbor : graph[from]) {
+    if (!visited[neighbor]) {
+      DFS_helper(graph, neighbor, visited, ret);
+    }
+  }
 }
 
 std::vector<int> DFS(std::vector<std::vector<int>> const& graph, int from) {
-  // COMPLETE AQUI
+  std::vector<bool> visited(graph.size(), false);
   std::vector<int> ret;
+  DFS_helper(graph, from, visited, ret);
   return ret;
 }
 
-std::vector<std::vector<int>> connected_components(
-  std::vector<std::vector<int>> const& graph) {
-  // COMPLETE AQUI
-  std::vector<std::vector<int>> ret;
-  return ret;
 
+std::vector<std::vector<int>> connected_components(std::vector<std::vector<int>> const& graph) {
+  std::vector<bool> visited(graph.size(), false);
+  std::vector<std::vector<int>> components;
+  
+  for (int i = 0; i < graph.size(); ++i) {
+    if (!visited[i]) {
+      std::vector<int> component;
+      DFS_helper(graph, i, visited, component); 
+      components.push_back(component);
+    }
+  }
+  
+  return components;
 }
 
 int n_connected_components(std::vector<std::vector<int>> const& graph) {
-  // COMPLETE AQUI
-  return 0;
+  std::vector<std::vector<int>> components = connected_components(graph);
+  return components.size();
 }
+
 
 #endif
